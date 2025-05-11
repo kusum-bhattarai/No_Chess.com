27e6d3b9a9b2520cc350d58f828f9edb036c314d
Kusum Bhattarai Sharma <xcm15@txstate.edu>
2025-05-11 12:20:10 -0500

    A little refactoring to make it more modular
    and reusable, added skill level

--- engine.py
+++ engine.py
@@ -1,10 +1,26 @@
 import subprocess
 
 class StockfishEngine:
-    def __init__(self):
+    def __init__(self, mode="intermediate"):
         #get the stockfish path, (use .exe for windows devices)
         executable = 'stockfish_binary/stockfish'
         
+        #modes mapped to depths
+        depth_map = {
+            "beginner": 8,
+            "intermediate": 12,
+            "advanced": 16
+        }
+
+        skill_map = {
+            "beginner": 5,
+            "intermediate": 10,
+            "advanced": 20
+        }
+
+        self.depth = depth_map.get(mode, 10) #10 in case mode is unrecognized
+        self.skill = skill_map.get(mode, 10) #10 in case not recognized
+
         #launch the Stockfish process
         self.engine = subprocess.Popen(
             executable,
@@ -16,17 +32,19 @@ class StockfishEngine:
         )
 
         #sending uci command
-        self.engine.stdin.write('uci/n') 
+        self.engine.stdin.write('uci\n') 
         self.engine.stdin.flush()
+        #wait for uciok response
+        self.read_until('uciok')
+
+        self.set_skill_level() #skill level set
 
         #making sure its ready
         self.engine.stdin.write('isready\n')
         self.engine.stdin.flush()
-
-
-        #waiting for uciok and readyok response
-        self.read_until('uciok')
+        #waiting for readyok response    
         self.read_until('readyok')
+
 
     def read_until(self, keyword):
         while True:
@@ -45,9 +63,13 @@ class StockfishEngine:
         self.engine.stdin.write(command + '\n')
         self.engine.stdin.flush()          
 
-    
-    def get_best_move(self, depth=15):
-        self.engine.stdin.write(f"go depth {depth}\n")
+    def set_skill_level(self):
+        self.engine.stdin.write(f"setoption name Skill Level value {self.skill}\n")
+        self.engine.stdin.flush()
+
+
+    def get_best_move(self):
+        self.engine.stdin.write(f"go depth {self.depth}\n")
         self.engine.stdin.flush()
 
         best_move = None
